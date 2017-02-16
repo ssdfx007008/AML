a37ff06044c5b1fa0d4525f25853d54dfb9f2556
Colin Li <li820970@gmail.com>
2017-02-15 21:10:41 -0600

    4-10b
    function to create distance matrix, read metadata, do 4-10b

--- hw3/4-10a.py
+++ hw3/4-10a.py
@@ -1,4 +1,8 @@
 import numpy as np
+from scipy.spatial import distance
+from matplotlib import pyplot as plt
+from sklearn import manifold
+from sklearn.decomposition import PCA
 import _pickle as cPickle
 
 def unpickle(file):
@@ -20,6 +24,10 @@ def read_files():
             ret[type].append(unsorted['data'][i])
     return np.array(ret)
 
+def read_meta():
+    return unpickle("cifar-10-batches-py/batches.meta")['label_names']
+    
+
 def compute_means(data):
     ret = np.empty((10,3072))
     for i in range(10):
@@ -28,9 +36,41 @@ def compute_means(data):
         ret[i] = np.divide(ret[i], data[i].shape[0])
     return ret
 
+def make_dist_matrix(inputvectors, distfunction):
+    ret = np.zeros( (inputvectors.shape[0],inputvectors.shape[0]) )
+    for i in range(inputvectors.shape[0]):
+        for j in range(inputvectors.shape[0]):
+            ret[i][j] = distfunction(inputvectors[i], inputvectors[j])
+    return ret
+
+def euclidean_dist(a, b):
+    return distance.euclidean(a,b)
+
+def partB(means, labels):
+    print('Begin part B')
+    dist_matrix = make_dist_matrix(means, euclidean_dist)
+    mds = manifold.MDS(n_components = 2, verbose = 1,max_iter=3000, n_jobs = 1, dissimilarity = 'precomputed')
+    #do NOT set n_jobs to anything > 1 - it makes fit hang for some reason
+    print('Fitting using MDS...')
+    mds_out = mds.fit(dist_matrix).embedding_
+    #print('Scaling')
+    #mds_out *= np.sqrt((means ** 2).sum()) / np.sqrt((mds_out ** 2).sum())
+    clf = PCA(n_components=2)
+    print('Transforming')
+    mds_out = clf.fit_transform(mds_out)
+    print('Plotting')
+    plt.scatter(mds_out[:,0], mds_out[:,1])
+    
+    for point in zip(mds_out[:,0], mds_out[:,1], range(0,10)):
+        plt.annotate(labels[point[2]], xy = point[0:2], xytext = (6, -6), textcoords = 'offset pixels')
+        
+    plt.show()
+
     
 sorted_data = read_files()
+label_names = read_meta()
 means = compute_means(sorted_data)
+partB(means, label_names)
 #
 # input function
 # 5 files-> pictures
