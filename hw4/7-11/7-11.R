642b793fae6d7bbe87f5bb5086a391cd1e908956
Colin Li <li820970@gmail.com>
2017-02-28 01:27:52 -0600

    graph export
    a-d

--- hw4/7-11/7-11.R
+++ hw4/7-11/7-11.R
@@ -11,7 +11,7 @@ abalone <- read.csv(file="abalone.data", header = TRUE)
 part_a.lm = lm(Rings ~ Length+Diameter+Height+Whole_weight+Shucked_weight+Viscera_weight+Shell_weight, data = abalone)
 part_a.res = resid(part_a.lm)
 
-plot(abalone$Rings, part_a.res, ylab="Residuals", xlab="Rings", main="Age") 
+plot(abalone$Rings, part_a.res, ylab="Residuals", xlab="Rings", main="A) Residuals Predicting Rings w/o Gender") 
 abline(0, 0) 
 
   # abalone <- lapply(abalone, function(x) {gsub("M", 1, x)}) These mess with the data set type and wreak havoc on lm, so i'm just preprocessing the data now
@@ -21,5 +21,20 @@ abline(0, 0)
 part_b.lm = lm(Rings ~ Sex+Length+Diameter+Height+Whole_weight+Shucked_weight+Viscera_weight+Shell_weight, data = abalone)
 part_b.res = resid(part_b.lm)
 
-plot(abalone$Rings, part_b.res, ylab="Residuals", xlab="Rings", main="Age") 
+plot(abalone$Rings, part_b.res, ylab="Residuals", xlab="Rings", main="B) Residuals Predicting Rings w/ Gender") 
 abline(0, 0) 
+
+
+part_c.lm = lm(log(Rings) ~ Length+Diameter+Height+Whole_weight+Shucked_weight+Viscera_weight+Shell_weight, data = abalone)
+part_c.res = resid(part_c.lm)
+
+plot(abalone$Rings, part_c.res, ylab="Residuals", xlab="Rings", main="C) Residuals Predicting log(Rings) w/o Gender") 
+abline(0, 0) 
+
+part_d.lm = lm(log(Rings) ~ Sex + Length+Diameter+Height+Whole_weight+Shucked_weight+Viscera_weight+Shell_weight, data = abalone)
+part_d.res = resid(part_d.lm)
+
+plot(abalone$Rings, part_d.res, ylab="Residuals", xlab="Rings", main="D) Residuals Predicting log(Rings) w/ Gender") 
+abline(0, 0) 
+
+
