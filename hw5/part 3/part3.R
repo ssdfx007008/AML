0647a0faf0c51a53baf090a637156c6c75bf5652
daishengliang <daishengliang@gmail.com>
2017-03-20 17:02:24 -0500

    part 3 R

--- hw5/part 3/part3.R
+++ hw5/part 3/part3.R
@@ -4,7 +4,7 @@ library(glmnet)
 setwd(getwd())
 
 # read input data
-raw_data <- read.csv("I2000.txt", header = FALSE, sep = " ")
+raw_data = read.csv("I2000.txt", header = FALSE, sep = " ")
 data = t(as.matrix(raw_data))
 
 raw_tissues = read.csv("tissues.txt", header = FALSE, sep = " ")
@@ -15,15 +15,15 @@ model = cv.glmnet(data, tissues, family = "binomial", type.measure = "deviance",
 
 #evaluate performance of model (deviance)
 plot(model)
-minlambda <- model$lambda.min
-deviance <- model$cvm[which(model$lambda == minlambda)]
-predictors <- model$nzero[which(model$lambda == minlambda)]
+minlambda = model$lambda.min
+deviance = model$cvm[which(model$lambda == minlambda)]
+predictors = model$nzero[which(model$lambda == minlambda)]
 
 set.seed(1)
-modelauc <- cv.glmnet(data, tissues, family = "binomial", type.measure = "auc", alpha = 1, nfolds = 5)
-minauclambda <- modelauc$lambda.min
+modelauc = cv.glmnet(data, tissues, family = "binomial", type.measure = "auc", alpha = 1, nfolds = 5)
+minauclambda = modelauc$lambda.min
 #get auc of model with the lowest deviance
-auc <- modelauc$cvm[which(model$lambda == minlambda)]
+auc = modelauc$cvm[which(model$lambda == minlambda)]
 
 minlambda
 deviance
